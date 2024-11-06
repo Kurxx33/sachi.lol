3fac59f89e0622879e60bac0f4015d0169aeedd9
Kurxx33 <168380676+Kurxx33@users.noreply.github.com>
2024-11-06 11:35:58 -0600

    Create service-worker.js

---
+++ docs/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        'https://r2.interrupted.me/F/wlp3.png',
+        'https://r2.interrupted.me/F/wlp3.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
