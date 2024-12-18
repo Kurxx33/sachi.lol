df396365af61a82d68860c93f38b5dc74a1fcec9
noire <168380676+trulyheartless@users.noreply.github.com>
2024-12-18 10:08:36 -0600

    Create funny.js

---
+++ docs/clarity/funny.js
@@ -0,0 +1,39 @@
+const body = document.getElementsByTagName('body')
+window.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && (event.key === 'S' || event.key === 's')) {
+        event.preventDefault()
+        body[0].innerHTML =
+          'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Xoru was here oh yeahhh kys nigger hahaha a hahaha xoru.lol hahha'
+    }
+    if (event.ctrlKey && event.key === 'C') {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Xoru was here oh yeahhh kys nigger hahaha a hahaha xoru.lol hahha'
+    }
+    if (event.ctrlKey && (event.key === 'E' || event.key === 'e')) {
+        event.preventDefault()
+        body[0].innerHTML =
+          'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Xoru was here oh yeahhh kys nigger hahaha a hahaha xoru.lol hahha'
+    }
+    if (
+        event.ctrlKey &&
+        (event.key === 'I' || event.key === 'i' || event.key === 'Ä\xB1')
+    ) {
+        event.preventDefault()
+        body[0].innerHTML =
+          'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Xoru was here oh yeahhh kys nigger hahaha a hahaha xoru.lol hahha'
+    }
+    if (event.ctrlKey && (event.key === 'K' || event.key === 'k')) {
+        event.preventDefault()
+        body[0].innerHTML =
+          'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Xoru was here oh yeahhh kys nigger hahaha xoru.lol hahha'
+    }
+    if (event.ctrlKey && (event.key === 'U' || event.key === 'u')) {
+        event.preventDefault()
+        body[0].innerHTML =
+          'nice try little nigga LOL >.< skiiding ass nigga grahhhhhh dumbyyyyy Xoru was here oh yeahhh kys nigger hahaha xoru.lol hahha'
+    }
+})
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault()
+})
